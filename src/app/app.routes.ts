0b06a7d7c66b83888eecca2f3698b35d01ed5534
cberleux <clement.berleux@gmail.com>
2024-11-03 23:14:13 +0100

    Update deployment

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 import { RecipeComponent } from './components/recipe/recipe.component';
-import { RecipeCardComponent } from './components/recipe-card/recipe-card.component';
 import { ListRecipesComponent } from './components/list-recipes/list-recipes.component';
 
 export const routes: Routes = [
